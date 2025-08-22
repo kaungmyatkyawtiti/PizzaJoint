be2a98e8319ffbd50b74b8cfe2628f3c7452cee7
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2025-08-22 19:59:52 +0630

    style(page): apply tailwind css

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,7 +1,16 @@
+import PizzaLogo from '../components/PizzaLogo';
+
 export default function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header className='flex items-center p-8'>
+      <div className="logo">
+        <PizzaLogo />
+      </div>
+      <div className="grow ml-2 text-lg text-white">
+        <h1
+          className='border-b-1 border-b-white/30 pb-3'
+        >Pizza Joint</h1>
+      </div>
+    </header>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,24 @@
+import { Link } from 'react-router-dom';
+import Header from './Header';
+
 export default function Home() {
   return (
     <div>
-      Home
+      <Header />
+      <div className="min-w-[800px] text-center my-10 py-10 text-white">
+        <h2
+          className='text-4xl mb-8'
+        >
+          Welcome to Pizza Joint
+        </h2>
+        <Link to="/base">
+          <button
+            className='text-white px-1 py-3 border-2 border-white mx-4 my-3'>
+            Create Your Pizza
+          </button>
+        </Link>
+      </div>
+
     </div>
   )
 }
